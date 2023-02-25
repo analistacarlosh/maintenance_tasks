7724b3949c038a780532648ad03abf922c45855b
Carlos Fructuoso <analista.carlosh@gmail.com>
2023-02-25 16:46:28 +0000

    Add unit test on Task service

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,20 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entity/task.entity';
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
   let service: TaskService;
+  const repositoryToken = getRepositoryToken(Task);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TaskService,
         {
-          provide: getRepositoryToken(Task),
+          provide: repositoryToken,
           useValue: {
-            save: jest.fn(),
+            save: jest.fn()
+              .mockImplementation((createTaskDto: CreateTaskDto) =>
+              Promise.resolve({ _id: '1', ...createTaskDto }),
+          ),
             find: jest.fn(),
           }
         }
@@ -22,10 +26,22 @@ describe('TaskService', () => {
     }).compile();
 
     service = module.get<TaskService>(TaskService);
-    
   });
 
-  it('should be defined', () => {
+  it('service should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Task::create', () => {
+
+    it('should create a task', async () => {
+      const task:CreateTaskDto = new CreateTaskDto();
+      task.title = 'title1';
+      task.summary = 'summary1';
+      const newTask: Task = await service.create(task);
+      expect(newTask).toEqual({ _id: '1', ...task });
+    });
+  
+  });
+
 });
