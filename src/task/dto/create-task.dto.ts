8f5051c1f030e6d63134521d87abc39c3a2ef850
Carlos Fructuoso <analista.carlosh@gmail.com>
2023-02-25 19:00:50 +0000

    Add Swagger to document the Api

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,10 +1,13 @@
-import { IsNotEmpty, Max, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateTaskDto {
+  @ApiProperty()
   @IsNotEmpty()
   @MaxLength(100)
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MaxLength(2500)
   summary: string;
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,27 +1,55 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entity/task.entity';
+import {
+  ApiCreatedResponse,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { ConstantStrings } from '../utils/constants/strings.constants';
+import { SwaggerDocumentationHelper } from '../utils/helpers/swagger-documentation.helper';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @ApiTags('task')
   @Post()
+  @ApiCreatedResponse({
+    status: 201,
+    description: ConstantStrings.swaggerDescription201Response,
+    schema: SwaggerDocumentationHelper.OkResponseObjectSchema(
+      getSchemaPath(CreateTaskDto),
+    ),
+  })
+  @ApiResponse({
+    status: 400,
+    description: ConstantStrings.swaggerDescription400BadRequest,
+  })
+  @ApiResponse({
+    status: 500,
+    description: ConstantStrings.swaggerDescription500Response,
+  })
   async create(@Body() createTaskDto: CreateTaskDto) {
     const newTask: Task = await this.taskService.create(createTaskDto);
     return { data: newTask };
   }
 
+  @ApiTags('task')
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: ConstantStrings.swaggerTaskDescription200Response,
+    schema: SwaggerDocumentationHelper.OkResponseObjectArraySchema(
+      getSchemaPath(CreateTaskDto),
+    ),
+  })
+  @ApiResponse({
+    status: 500,
+    description: ConstantStrings.swaggerDescription500Response,
+  })
   async findAll() {
     const taskList: Task[] = await this.taskService.findAll();
     return { data: taskList };
